3b5b5d1fc9f119d5820a6524e35b428390b93ea8
Lucca Piovezan <70606998+LuccaPiovezan@users.noreply.github.com>
2021-04-29 12:07:28 -0300

    br.softblue.jogodaforca
    Main

---
+++ Main.java
@@ -0,0 +1,13 @@
+package br.com.softblue.jogoDaForca;
+
+import br.com.softblue.jogoDaForca.Game.Game;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Game game = new Game();
+		game.start();
+	}
+
+}
